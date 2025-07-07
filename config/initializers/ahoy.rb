430d92534dc4a20c4496b2fea33943e094ab973c
KvngJamesNG <adesinaadewale28@yahoo.com>
2025-07-07 02:35:10 +0100

    i also just added a ahoy gem and config to track users and geocode in and out of the website

--- config/initializers/ahoy.rb
+++ config/initializers/ahoy.rb
@@ -8,3 +8,8 @@ Ahoy.api = false
 # we recommend configuring local geocoding as well
 # see https://github.com/ankane/ahoy#geocoding
 Ahoy.geocode = false
+
+Ahoy.configure do |config|
+  config.geocode = true        # enables IP-based location tracking
+  config.track_bots = true   # skip Googlebot, Bingbot, etc.
+end
