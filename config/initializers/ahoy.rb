c1d9e910798fe2ecdd99eee5d240d774f600cba2
KvngJamesNG <adesinaadewale28@yahoo.com>
2025-07-07 03:07:54 +0100

    had issue with NoMethodError: undefined method configure for module Ahoy (NoMethodError)

--- config/initializers/ahoy.rb
+++ config/initializers/ahoy.rb
@@ -7,8 +7,6 @@ Ahoy.api = true
 # set to true for geocoding (and add the geocoder gem to your Gemfile)
 # we recommend configuring local geocoding as well
 # see https://github.com/ankane/ahoy#geocoding
-
-Ahoy.configure do |config|
-  config.geocode = true        # enables IP-based location tracking
-  config.track_bots = true   # skip Googlebot, Bingbot, etc.
-end
+ # Enable JS tracking
+Ahoy.geocode = true   # Disable geocoding (or true if you want it)
+Ahoy.track_bots = true # Exclude bots (set to true if you want to track them)
